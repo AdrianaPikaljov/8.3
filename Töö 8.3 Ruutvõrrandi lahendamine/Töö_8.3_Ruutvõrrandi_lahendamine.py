7415fc482370e398fa77b3730851ef389356baf3
adri <pikaljovadri@gmail.com>
2025-05-20 12:44:56 +0300

    Update Töö_8.3_Ruutvõrrandi_lahendamine.py

--- Töö 8.3 Ruutvõrrandi lahendamine/Töö_8.3_Ruutvõrrandi_lahendamine.py
+++ Töö 8.3 Ruutvõrrandi lahendamine/Töö_8.3_Ruutvõrrandi_lahendamine.py
@@ -1,31 +1,24 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from tkinter import *
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-def lahenda_vorrand():
+def lahenda_võrrand():
     try:
         a = float(sisse1.get())
         b = float(sisse2.get())
         c = float(sisse3.get())
-
         D = b**2 - 4*a*c
-
         if D > 0:
             x1 = (-b + np.sqrt(D)) / (2*a)
             x2 = (-b - np.sqrt(D)) / (2*a)
-            tulemus_label.config(text=f"Lahendus: D={D:.2f}, x1 = {x1:.2f}, x2 = {x2:.2f}")
-            joonista_graafik(a, b, c, x1, x2)
+            result_label.config(text=f"Lahendus: x1 = {x1:.2f}, x2 = {x2:.2f}", fg='green')
         elif D == 0:
             x = -b / (2*a)
-            tulemus_label.config(text=f"Lahendus: x = {x:.2f}")
-            joonista_graafik(a, b, c, x)
+            result_label.config(text=f"Lahendus: x = {x:.2f}", fg='green')
         else:
-            tulemus_label.config(text="Pole reaalset lahendust")
-            joonista_graafik(a, b, c)
-
-    except ValueError:
-        tulemus_label.config(text="Sisesta õiged väärtused")
+            result_label.config(text="Pole reaalseid lahendusi.", fg='green')
+    except:
+        result_label.config(text="Sisesta õiged väärtused.", fg='red')
 
 def joonista_graafik():
     try:
@@ -37,8 +30,7 @@ def joonista_graafik():
         y = a * x**2 + b * x + c
         plt.figure(figsize=(6, 4))
         plt.plot(x, y, label=f'{a}x² + {b}x + {c}', color='green')
-        plt.axhline(0, color='black',linewidth=1)
-        plt.axvline(0, color='black',linewidth=1)
+        plt.axhline(0, color='black', linewidth=1)
         plt.title('Graafik: Ruutvõrrandi f(x)')
         plt.xlabel('x')
         plt.ylabel('f(x)')
@@ -46,46 +38,34 @@ def joonista_graafik():
         plt.legend()
         plt.show()
     except ValueError:
-        result_label.config(text="Sisesta õiged väärtused.")
+        result_label.config(text="Sisesta õiged väärtused.", fg='red')
 
 aken = Tk()
-aken.title("Ruutvõrrandi Lahendamine")
-aken.geometry('700x600')
-aken.configure(bg="grey")
+aken.title('Ruutvõrrandi lahendamine')
+aken.geometry('500x400')
+aken.configure(bg='lightgrey')
+pealkiri = Label(aken, text='Ruutvõrrandi lahendamine', bg='lightblue', font=('Arial', 16), fg='green')
+pealkiri.place(x=120, y=20)
 
-
-pealkiri = Label(aken, text="Ruutvõrrandi lahendamine", bg='lightgrey', font=('Arial', 16), fg='green')
-pealkiri.place(x=220, y=20)
-
-sisse1 = Entry(aken, font=("Arial", 15), bg="lightblue", fg="black", width=4)
-sisse1.place(x=200, y=100)
-
+sisse1 = Entry(aken, font=("Arial", 15), bg="lightblue", fg="green", width=4)
+sisse1.place(x=10, y=100)
 kiri1 = Label(aken, text="x² +", font=("Arial", 15), bg="lightgrey", fg="green")
-kiri1.place(x=250, y=100)
-
-sisse2 = Entry(aken, font=("Arial", 15), bg="lightblue", fg="black", width=4)
-sisse2.place(x=300, y=100)
-
+kiri1.place(x=60, y=100)
+sisse2 = Entry(aken, font=("Arial", 15), bg="lightblue", fg="green", width=4)
+sisse2.place(x=100, y=100)
 kiri2 = Label(aken, text="x +", font=("Arial", 15), bg="lightgrey", fg="green")
-kiri2.place(x=350, y=100)
-
-sisse3 = Entry(aken, font=("Arial", 15), bg="lightblue", fg="black", width=4)
-sisse3.place(x=400, y=100)
-
+kiri2.place(x=160, y=100)
+sisse3 = Entry(aken, font=("Arial", 15), bg="lightblue", fg="green", width=4)
+sisse3.place(x=200, y=100)
 kiri3 = Label(aken, text="= 0", font=("Arial", 15), bg="lightgrey", fg="green")
-kiri3.place(x=450, y=100)
+kiri3.place(x=260, y=100)
+sisse4 = Button(aken, text="Lahenda", font=("Arial", 15), bg="darkgreen", fg="white", relief="raised", width=10, command=lahenda_võrrand)
+sisse4.place(x=320, y=90)
 
-sisse4 = Button(aken, text="Lahenda", font=("Arial", 15), bg="darkgreen", fg="white", relief="raised", width=10, command=lahenda_vorrand)
-sisse4.place(x=500, y=90)
+result_label = Label(aken, text="= ?", font=("Arial", 15), bg="lightyellow", fg="green")
 
-sisse5 = Button(aken, text="Graafik", font=("Arial", 15), bg="darkgreen", fg="white", relief="raised", width=10, command=joonista_graafik)
-sisse5.place(x=500, y=140)
+result_label.place(x=100, y=200)
 
-tulemus_label = Label(aken, text="= ?", font=("Arial", 15), bg="lightgrey", fg="green")
-tulemus_label.place(x=300, y=150)
+sisse5 = Button(aken, text="Joonista graafik", font=("Arial", 15), bg="darkgreen", fg="white", relief="raised", width=15, command=joonista_graafik)
+sisse5.place(x=320, y=150)
 
-
-
-
-
-aken.mainloop()
